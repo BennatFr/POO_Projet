17aba53c4e293dae6e42e283d06e595b68891567
BennatFr <48688527+BennatFr@users.noreply.github.com>
2022-11-24 11:36:07 +0100

    Initialisation du projet VS Code
    Création du Form.h & Form.cpp
    
    Avec code Main

---
+++ POO_Projet/POO_Projet/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThreadAttribute]
+int __clrcall Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	POOProjet::MyForm monFormulaire;
+	Application::Run(% monFormulaire);
+
+	return 0;
+}
